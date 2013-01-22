b4b008ccb86648685cc3a19849512545d92656b5
freem-trg <freem.trg@gmail.com>
2013-01-22 17:37:47 +0400

    Update students.cpp

--- students.cpp
+++ students.cpp
@@ -10,10 +10,10 @@ int main(int argc, char *argv[])
 
     const int STUDENTS_NUM = 10;
 
-	//For MPI
-	MPI_Status status;
-	int pid;	
-	int TAG = 0;
+    //For MPI
+    MPI_Status status;
+    int pid;	
+    int TAG = 0;
     int threads = 0;
     int students[STUDENTS_NUM] = { 0 };
     int tmp = 0;
@@ -37,16 +37,16 @@ int main(int argc, char *argv[])
         }
     }
 
-	//Enter the MPI:
-	MPI_Init( &argc, &argv );
+    //Enter the MPI:
+    MPI_Init( &argc, &argv );
     MPI_Comm_size(MPI_COMM_WORLD, &threads);
-	MPI_Comm_rank( MPI_COMM_WORLD, &pid);
-	if ( pid != 0){
+    MPI_Comm_rank( MPI_COMM_WORLD, &pid);
+    if ( pid != 0){
         //Student code here:
         cout << pid << " have a ticket!" << endl;
         MPI_Send( &pid, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
-	} else {
-        //Conductor code here:
+    } else {
+         //Conductor code here:
         int student = 0;
         for (int i = 0; i < STUDENTS_NUM; i++){
             MPI_Recv( &student, 1, MPI_INT, students[i], TAG, MPI_COMM_WORLD, &status );
